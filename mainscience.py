bb2fe185e50f52a7e58e57e952a7bfa1181344f5
MelizaVill <94480230+MelizaVill@users.noreply.github.com>
2022-02-18 23:25:37 -0500

    creacion del py

---
+++ mainscience.py
@@ -0,0 +1,13 @@
+def download_pubmed(Ecuador genomics): 
+    """Docstring download_pubmed"""
+    
+    
+    return 
+
+def mining_pubs(tipo):
+    """Docstring mining_pubs"""
+    if tipo == "AD":
+        
+    return 
+
+    
